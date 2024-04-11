bba73989face9eb9c08c2d53357bf4171ed11d1a
mjfwebb <michael@w3bb.net>
2024-04-11 21:57:28 +0200

    update

--- src/main.c
+++ src/main.c
@@ -50,7 +50,8 @@ typedef struct
   direction_fields;
   float speed;
   int layer;
-  bool dead;
+  bool selected;
+  char *name;
 } Entity;
 
 typedef struct
@@ -71,6 +72,7 @@ typedef struct
   int current_time;
   int last_update_time;
   float speed;
+  float prev_speed;
   float delta_time;
   float animated_time;
   SDL_Renderer *renderer;
@@ -104,7 +106,7 @@ typedef struct
 
 void render_entity(RenderContext *render_context, Entity *entity);
 
-Entity make_entity(char *image_path, SDL_Renderer *renderer)
+Entity make_entity(char *image_path, SDL_Renderer *renderer, char *name)
 {
   SDL_Surface *image = SDL_LoadBMP(image_path);
   assert(image);
@@ -124,7 +126,8 @@ Entity make_entity(char *image_path, SDL_Renderer *renderer)
       .texture = texture,
       .h = height,
       .w = width,
-      .dead = false,
+      .selected = false,
+      .name = name,
   };
 
   // Surface no longer needed after the texture is created
@@ -154,9 +157,13 @@ void draw_texture(RenderContext *render_context, Entity *entity)
   }
 }
 
-void draw_text(RenderContext *render_context, char *text, float x, float y)
+void draw_text(RenderContext *render_context, char *text, int font_index, float x, float y, bool relative_to_camera)
 {
-  TTF_Font *font = render_context->fonts[0];
+  int window_w;
+  int window_h;
+  SDL_GetWindowSizeInPixels(render_context->window, &window_w, &window_h);
+
+  TTF_Font *font = render_context->fonts[font_index];
   SDL_Color White = {255, 255, 255};
   SDL_Surface *text_surface = TTF_RenderText_Solid(font, text, White);
   if (!text_surface)
@@ -170,11 +177,13 @@ void draw_text(RenderContext *render_context, char *text, float x, float y)
     fprintf(stderr, "could not create text texture: %s\n", SDL_GetError());
   }
 
+  float zoom = relative_to_camera ? render_context->render_zoom : 1.0f;
+
   SDL_FRect text_rect = {
-      .w = (float)text_surface->w,
-      .h = (float)text_surface->h,
-      .x = x,
-      .y = y,
+      .w = (float)text_surface->w * zoom,
+      .h = (float)text_surface->h * zoom,
+      .x = relative_to_camera ? (x - render_context->render_camera_x) * render_context->render_zoom + window_w / 2 : x,
+      .y = relative_to_camera ? (y - render_context->render_camera_y - (30.0f)) * render_context->render_zoom + window_h / 2 : y,
   };
 
   SDL_RenderCopyF(render_context->renderer, text_texture, NULL, &text_rect);
@@ -187,16 +196,16 @@ void render_debug_info(RenderContext *render_context, MouseState *mouse_state)
   char text[128];
 
   sprintf(text, "fps: %.2f", render_context->fps);
-  draw_text(render_context, text, 10.0f, 10.0f);
+  draw_text(render_context, text, 0, 10.0f, 10.0f, false);
 
   sprintf(text, "mouse state: %d, button: %d, clicks: %d", mouse_state->state, mouse_state->button, mouse_state->clicks);
-  draw_text(render_context, text, 10.0f, 40.0f);
+  draw_text(render_context, text, 0, 10.0f, 40.0f, false);
 
   sprintf(text, "prev mouse state: %d", mouse_state->prev_state);
-  draw_text(render_context, text, 10.0f, 70.0f);
+  draw_text(render_context, text, 0, 10.0f, 70.0f, false);
 
   sprintf(text, "camera zoom: %.1f", render_context->camera.zoom);
-  draw_text(render_context, text, 10.0f, 100.0f);
+  draw_text(render_context, text, 0, 10.0f, 100.0f, false);
 }
 
 float Spring__update(Spring *spring, float target)
@@ -206,41 +215,38 @@ float Spring__update(Spring *spring, float target)
   spring->velocity *= spring->friction;
   return spring->current += spring->velocity;
 }
-void render(RenderContext *render_context, Entity *entities, int entities_count, int *entities_render_order, int entities_render_order_count)
+void render(RenderContext *render_context, Entity *entities, int entities_count)
 {
   int window_w;
   int window_h;
   SDL_GetWindowSizeInPixels(render_context->window, &window_w, &window_h);
 
-  // render_camera(render_context);
-
-  for (int i = 0; i < entities_count; i++)
+  for (int entity_i = 0; entity_i < entities_count; entity_i++)
   {
-    if (entities[entities_render_order[i]].texture && entities[entities_render_order[i]].dead == false)
+    if (entities[entity_i].texture)
     {
-      // detect_window_edge_collision(&entities[entities_render_order[i]], (Rect){.w = window_w, .h = window_h});
-
-      // for (int j = 0; j < entities_count; j++)
-      // {
-      //   if (entities[entities_render_order[j]].texture)
-      //   {
-      //     if (i != j)
-      //     {
-      //       bool collision = detect_entity_collision(&entities[entities_render_order[i]], &entities[entities_render_order[j]]);
-      //       if (collision)
-      //       {
-      //         // printf("entity %d collided with entity %d\n", i, j);
-      //       }
-      //     }
-      //   }
-      // }
-
-      render_entity(render_context, &entities[entities_render_order[i]]);
+      render_entity(render_context, &entities[entity_i]);
     }
   }
 
   SDL_RenderPresent(render_context->renderer);
 }
+
+SDL_FRect render_rect(RenderContext *render_context, float w, float h, float x, float y)
+{
+  int window_w;
+  int window_h;
+  SDL_GetWindowSizeInPixels(render_context->window, &window_w, &window_h);
+
+  SDL_FRect rect = {
+      .w = w * render_context->render_zoom,
+      .h = h * render_context->render_zoom,
+      .x = ((x - render_context->render_camera_x) * render_context->render_zoom + window_w / 2),
+      .y = ((y - render_context->render_camera_y) * render_context->render_zoom + window_h / 2),
+  };
+
+  return rect;
+};
 
 void render_entity(RenderContext *render_context, Entity *entity)
 {
@@ -248,6 +254,30 @@ void render_entity(RenderContext *render_context, Entity *entity)
   entity->y += entity->direction_y * (render_context->delta_time * render_context->speed);
 
   draw_texture(render_context, entity);
+
+  if (entity->selected)
+  {
+    int window_w;
+    int window_h;
+    SDL_GetWindowSizeInPixels(render_context->window, &window_w, &window_h);
+
+    SDL_FRect rect = {
+        .w = (entity->w + 10.0f) * render_context->render_zoom,
+        .h = (entity->h + 10.0f) * render_context->render_zoom,
+        .x = ((entity->x - render_context->render_camera_x) * render_context->render_zoom + window_w / 2) - 10.0f * 0.5f,
+        .y = ((entity->y - render_context->render_camera_y) * render_context->render_zoom + window_h / 2) - 10.0f * 0.5f,
+    };
+
+    SDL_SetRenderDrawColor(render_context->renderer,
+                           255, 255, 255, 255);
+    SDL_RenderDrawRectF(render_context->renderer, &rect);
+
+    // SDL_RenderSetClipRect(render_context.renderer, NULL);
+    // int result = SDL_RenderFillRect(render_context.renderer, NULL);
+    // assert(result == 0);
+  }
+
+  draw_text(render_context, entity->name, 1, entity->x, entity->y, true);
 }
 
 TTF_Font *load_font(const char *font_file_path, int font_size)
@@ -275,6 +305,8 @@ int main(int argc, char *args[])
     return 1;
   }
 
+  SDL_SetHint(SDL_HINT_RENDER_SCALE_QUALITY, "best");
+
   window = SDL_CreateWindow(
       "Cultivation Sim",
       SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
@@ -294,25 +326,35 @@ int main(int argc, char *args[])
   }
 
   Entity entities[] = {
-      make_entity("assets/lamb2.bmp", renderer),
-      make_entity("assets/stone.bmp", renderer),
-      make_entity("assets/lamb.bmp", renderer),
-      make_entity("assets/stone.bmp", renderer),
-      make_entity("assets/lamb.bmp", renderer),
-      make_entity("assets/stone.bmp", renderer),
-      make_entity("assets/lamb2.bmp", renderer),
-      make_entity("assets/fish.bmp", renderer),
-      make_entity("assets/fish.bmp", renderer),
-      make_entity("assets/fish.bmp", renderer),
-      make_entity("assets/fish.bmp", renderer),
-      make_entity("assets/stone.bmp", renderer),
+      make_entity("assets/lamb2.bmp", renderer, "pushqrdx"),
+      make_entity("assets/stone.bmp", renderer, "Athano"),
+      make_entity("assets/lamb.bmp", renderer, "AshenHobs"),
+      make_entity("assets/stone.bmp", renderer, "adrian_learns"),
+      make_entity("assets/lamb.bmp", renderer, "RVerite"),
+      make_entity("assets/stone.bmp", renderer, "Orshy"),
+      make_entity("assets/lamb2.bmp", renderer, "ruggs888"),
+      make_entity("assets/fish.bmp", renderer, "Xent12"),
+      make_entity("assets/fish.bmp", renderer, "nuke_bird"),
+      make_entity("assets/stone.bmp", renderer, "Kasper_573"),
+      make_entity("assets/fish.bmp", renderer, "SturdyPose"),
+      make_entity("assets/stone.bmp", renderer, "coffee_lava"),
+      make_entity("assets/stone.bmp", renderer, "goudacheeseburgers"),
+      make_entity("assets/stone.bmp", renderer, "ikiwixz"),
+      make_entity("assets/lamb2.bmp", renderer, "NixAurvandil"),
+      make_entity("assets/lamb2.bmp", renderer, "smilingbig"),
+      make_entity("assets/lamb.bmp", renderer, "tk_dev"),
+      make_entity("assets/lamb2.bmp", renderer, "realSuperku"),
+      make_entity("assets/stone.bmp", renderer, "Hoby2000"),
+      make_entity("assets/stone.bmp", renderer, "CuteMath"),
+      make_entity("assets/stone.bmp", renderer, "forodor"),
+      make_entity("assets/stone.bmp", renderer, "Azenris"),
+      make_entity("assets/stone.bmp", renderer, "collector_of_stuff"),
   };
 
   TTF_Font *fonts[] = {
       load_font("assets/OpenSans-Regular.ttf", 32),
+      load_font("assets/OpenSans-Regular.ttf", 16),
   };
-
-  int entities_render_order[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
 
   RenderContext render_context = {
       .renderer = renderer,
@@ -428,21 +470,17 @@ int main(int argc, char *args[])
           render_context.speed = SDL_max(render_context.speed - 100.0f, 0);
           break;
 
-          // case SDLK_w:
-          //   render_context.camera.y -= 50.0f / render_context.render_zoom;
-          //   break;
-
-          // case SDLK_s:
-          //   render_context.camera.y += 50.0f / render_context.render_zoom;
-          //   break;
-
-          // case SDLK_a:
-          //   render_context.camera.x -= 50.0f / render_context.render_zoom;
-          //   break;
-
-          // case SDLK_d:
-          //   render_context.camera.x += 50.0f / render_context.render_zoom;
-          //   break;
+        case SDLK_SPACE:
+          if (render_context.prev_speed > 0)
+          {
+            render_context.speed = render_context.prev_speed;
+            render_context.prev_speed = 0;
+          }
+          else
+          {
+            render_context.prev_speed = render_context.speed;
+            render_context.speed = 0;
+          }
 
         default:
           break;
@@ -455,19 +493,9 @@ int main(int argc, char *args[])
 
       if (mouse_state.state == SDL_PRESSED && mouse_state.prev_state == SDL_RELEASED)
       {
-        for (int entity_i = array_count(entities) - 1; entity_i >= 0; entity_i--)
+        for (int entity_i = 0; entity_i < array_count(entities); entity_i++)
         {
-          if (entities[entities_render_order[entity_i]].dead)
-          {
-            continue;
-          }
-
-          SDL_FRect rect = {
-              .x = entities[entities_render_order[entity_i]].x,
-              .y = entities[entities_render_order[entity_i]].y,
-              .w = entities[entities_render_order[entity_i]].w,
-              .h = entities[entities_render_order[entity_i]].h,
-          };
+          SDL_FRect rect = render_rect(&render_context, entities[entity_i].w, entities[entity_i].h, entities[entity_i].x, entities[entity_i].y);
 
           SDL_FPoint point = {
               .x = mouse_state.x,
@@ -476,7 +504,7 @@ int main(int argc, char *args[])
 
           if (SDL_PointInFRect(&point, &rect))
           {
-            entities[entities_render_order[entity_i]].dead = true;
+            entities[entity_i].selected = !entities[entity_i].selected;
             break;
           }
         }
@@ -528,11 +556,10 @@ int main(int argc, char *args[])
     // SDL_RenderSetClipRect(render_context.renderer, NULL);
     int result = SDL_RenderFillRect(render_context.renderer, NULL);
     assert(result == 0);
-    // update(entities, array_count(entities));
 
     render_debug_info(&render_context, &mouse_state);
 
-    render(&render_context, entities, array_count(entities), entities_render_order, array_count(entities));
+    render(&render_context, entities, array_count(entities));
   }
 
   SDL_DestroyWindow(window);
